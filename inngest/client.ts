67888e5601cdc9df3c37e9e1878cebd50810a8fd
sumitsingh6122 <sumitsingh6122@gmail.com>
2025-03-24 07:38:41 +0530

    first

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,4 +1,5 @@
 import { Inngest } from "inngest";
 
-// Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
+
+inngest.setEventKey(process.env.INNGEST_EVENT_KEY || '');
