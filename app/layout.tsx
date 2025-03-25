d5305363f9daaf42b4d3e752858165c95838911a
SumitSingh6122 <159093892+SumitSingh6122@users.noreply.github.com>
2025-03-25 20:51:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" className="dark" suppressHydrationWarning={true}>
       <body
         className={outfit.className}
       ><NextThemesProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
